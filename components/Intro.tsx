d5e482d895c41f49d1ca42535b5d317d4b91bd90
Yaser Elkhabiry <yaser.elkhabiry@outlook.com>
2025-07-23 15:04:34 +0200

    Förbättrad visning av profilbild

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import React, { Children } from "react";
+import React, { useState } from "react";
 import { ContainerScroll } from "./ui/ContainerScroll";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 function Intro() {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const { scrollYProgress } = useScroll();
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, 10]);
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 1.05]);
+
   const titleComponent = "";
   return (
     <div id="about">
       <div className="flex flex-col">
         <ContainerScroll titleComponent={titleComponent}>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center px-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:pt-16 items-center px-6 ">
             <div className="text-center md:text-left space-y-6">
               <h3 className="text-3xl text-purple font-bold">About Me</h3>
               <p className="text-white text-xl leading-relaxed">
@@ -23,10 +29,11 @@ function Intro() {
               </p>
             </div>
             <div className="flex justify-center">
-              <img
-                src="/test2.jpg"
-                alt="b5"
-                className="max-w-xs md:max-w-sm lg:max-w-md rounded-full"
+              <motion.img
+                src="/profile.jpg"
+                style={{ rotate, scale }}
+                className="max-w-xs md:max-w-sm rounded-full cursor-pointer"
+                whileHover={{ scale: 1.1 }}
               />
             </div>
           </div>
